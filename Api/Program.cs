952cb7b7b33d7d423a40d54471d49019af659fae
Gabriel Mongeon <gabrielmongeon@hotmail.com>
2024-12-28 14:03:42 -0500

    Add Supabase client integration
    Introduce Supabase client to the project:
    - Add Supabase package reference (v1.1.1) in `Api.csproj`
    - Configure Supabase in `Program.cs` with environment variables
    - Create `SupabaseConfiguration` class for URL and Key management

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,15 @@
+using Api;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Supabase;
+
+var options = new SupabaseOptions
+{
+    AutoRefreshToken = true,
+    AutoConnectRealtime = true,
+    // SessionHandler = new SupabaseSessionHandler() <-- This must be implemented by the developer
+};
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
@@ -8,6 +17,12 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        var supabaseConfig = new SupabaseConfiguration
+        {
+            Url = Environment.GetEnvironmentVariable("SUPABASE_URL", EnvironmentVariableTarget.Process) ?? string.Empty,
+            Key = Environment.GetEnvironmentVariable("SUPABASE_KEY", EnvironmentVariableTarget.Process) ?? string.Empty
+        };
+        services.AddSingleton(provider => new Supabase.Client(supabaseConfig.Url ?? string.Empty, supabaseConfig.Key ?? string.Empty, options));
     })
     .Build();
 
