976bef0b96e9c15307edabb7bf55e210f08d0c88
Gabriel Mongeon <gabrielmongeon@hotmail.com>
2025-01-03 12:55:25 -0500

    Replace MudChart with ApexChart in EntriesChart
    Updated Client.csproj to include new package references for Blazor-ApexCharts, Microsoft.AspNetCore.Components.WebAssembly, Microsoft.AspNetCore.Components.WebAssembly.DevServer, and MudBlazor. Modified EntriesChart.razor to use ApexChart instead of MudChart, including adding ApexCharts namespace, defining ApexPointSeries, setting Height parameter, and initializing ApexChartOptions. Updated Home.razor to adjust EntriesChart component. Updated Program.cs to include ApexCharts namespace and service.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+using ApexCharts;
 using Essence.Client;
 using Essence.Client.Entries;
 using Microsoft.AspNetCore.Components.Web;
@@ -9,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
+builder.Services.AddApexCharts();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
